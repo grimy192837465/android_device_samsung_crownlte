b8d09a4fc610f2acb824de004b09b89d70b5f354
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-09-20 03:29:58 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,8 @@ DEVICE_PATH := device/samsung/crownlte
 TARGET_OTA_ASSERT_DEVICE := crownltexx,crownlteks,crownlte
 
 # Kernel
-TARGET_KERNEL_CONFIG := exynos9810-crownlte_defconfig
-TARGET_KERNEL_SOURCE := yarpiin_defconfig
+TARGET_KERNEL_CONFIG := yarpiin_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/universal9810-crown
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
